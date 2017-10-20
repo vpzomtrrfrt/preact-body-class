dc4ef76d5f9d3e90fb77520d534d68fb7562ba17
Iestyn Williams <iest@me.com>
2017-10-20 10:27:13 +0100

    Harden test (and run through prettier)

--- test/browser.spec.js
+++ test/browser.spec.js
@@ -2,57 +2,111 @@
 /*global global, describe, it, afterEach, before, after */
 'use strict';
 var expect = require('expect.js'),
-    enzyme = require('enzyme'),
-    React = require('react'),
-    ReactDOM = require('react-dom'),
-    BodyClassName = require('../'),
-    jsdom = require('jsdom').jsdom;
+  enzyme = require('enzyme'),
+  React = require('react'),
+  ReactDOM = require('react-dom'),
+  BodyClassName = require('../'),
+  jsdom = require('jsdom').jsdom;
 
-describe('BodyClassName (in a browser)', function () {
-  global.beforeEach(function () {
+describe('BodyClassName (in a browser)', function() {
+  global.beforeEach(function() {
     BodyClassName.canUseDOM = true;
   });
 
-  it('changes the document body class name on mount', function () {
+  it('changes the document body class name on mount', function() {
     var className = 'hello world';
-    var Component = enzyme.mount(React.createElement(BodyClassName, {className: className}));
+    var Component = enzyme.mount(
+      React.createElement(BodyClassName, { className: className })
+    );
     expect(global.document.body.className).to.equal(className);
+    Component.unmount();
   });
 
-  it('does not erase existing body class names', function () {
-    global.document.body.className = 'testing'
+  it('does not erase existing body class names', function() {
+    global.document.body.className = 'testing';
     var className = 'hello world';
-    var Component = enzyme.mount(React.createElement(BodyClassName, {className: className}));
+    var Component = enzyme.mount(
+      React.createElement(BodyClassName, { className: className })
+    );
     expect(global.document.body.className).to.equal('testing hello world');
+    Component.unmount();
   });
 
-  it('supports nesting, gathering all classNames used', function (done) {
-    global.document.body.className = ''
+  it('changes the document body class name on unmount', function() {
+    global.document.body.className = 'leavemehere';
+    var className = 'yoohoo';
+    var Component = enzyme.mount(
+      React.createElement(BodyClassName, { className: className })
+    );
+    expect(global.document.body.className).to.equal('leavemehere yoohoo');
+    Component.unmount();
+    expect(global.document.body.className).to.equal('leavemehere');
+  });
+
+  it('keeps its own internal state', function() {
+    global.document.body.className = 'alreadyhere';
+    var Component = enzyme.mount(
+      React.createElement(BodyClassName, { className: 'test1' })
+    );
+    expect(global.document.body.className).to.equal('alreadyhere test1');
+    Component.unmount();
+  });
+
+  it('and its own internal state works as expected', function() {
+    global.document.body.className = 'alreadyhere';
+    var Component = enzyme.mount(
+      React.createElement(BodyClassName, { className: 'test1' })
+    );
+    expect(global.document.body.className).to.equal('alreadyhere test1');
+    var Component2 = enzyme.mount(
+      React.createElement(BodyClassName, { className: 'test2' })
+    );
+    expect(global.document.body.className).to.equal('alreadyhere test1 test2');
+    Component.unmount();
+    Component2.unmount();
+  });
+
+  it('supports nesting, gathering all classNames used', function(done) {
+    global.document.body.className = '';
     var called = false;
-    var firstName = 'hello world';
-    var secondName = 'foo bar';
-    class Component1 extends React.Component{
+    var firstName = 'foo';
+    var secondName = 'bar';
+    var thirdName = 'baz';
+    class Third extends React.Component {
       componentDidMount() {
-        setTimeout(function () {
+        setTimeout(function() {
           expect(called).to.be(true);
-          expect(global.document.body.className).to.equal(firstName + ' ' + secondName);
+          expect(global.document.body.className).to.equal(
+            [thirdName, secondName, firstName].join(' ')
+          );
           done();
         });
       }
       render() {
-        return React.createElement(BodyClassName, {className: firstName});
+        return React.createElement(BodyClassName, { className: firstName });
       }
     }
-    class Component2 extends React.Component {
+    class Second extends React.Component {
+      render() {
+        return React.createElement(
+          BodyClassName,
+          { className: secondName },
+          React.DOM.div(null, React.createElement(Third))
+        );
+      }
+    }
+    class First extends React.Component {
       componentDidMount() {
         called = true;
       }
       render() {
-        return React.createElement(BodyClassName, {className: secondName},
-          React.DOM.div(null, React.createElement(Component1))
+        return React.createElement(
+          BodyClassName,
+          { className: thirdName },
+          React.DOM.div(null, React.createElement(Second))
         );
       }
     }
-    enzyme.mount(React.createElement(Component2), global.document.body);
+    enzyme.mount(React.createElement(First), global.document.body);
   });
 });
