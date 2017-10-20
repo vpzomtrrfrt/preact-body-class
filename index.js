4cf7d567f637c2057f1379de9461f4e00c8c21a5
Iestyn Williams <iest@me.com>
2017-10-20 09:42:21 +0100

    Refactor handling of "untracked" classnames

--- index.js
+++ index.js
@@ -4,44 +4,43 @@ var React = require('react');
 var withSideEffect = require('react-side-effect');
 var PropTypes = require('prop-types');
 
-let classNameCache = []
-
 function splitClassName(className) {
-  return className.split(/\s+/)
+  return className.split(/\s+/);
 }
 
 function reducePropsToState(propsList) {
-  return propsList.map(function(props) {
-    return props.className;
-  }).filter(function (value, index, self) {
-    return self.indexOf(value) === index;
-  }).join(' ');
+  return propsList
+    .map(function(props) {
+      return props.className;
+    })
+    .filter(function(value, index, self) {
+      return self.indexOf(value) === index;
+    })
+    .join(' ');
 }
 
 function handleStateChangeOnClient(stringClassNames) {
-  // get current body class as array
-  let currentClassNames = splitClassName(document.body.className)
-  
-  // remove all past class names
-  for (let i = 0; i < classNameCache.length; i++) {
-    const idx = currentClassNames.indexOf(classNameCache[i]);
-    if (idx !== -1) {
-      currentClassNames.splice(idx, 1);
-    }
-  }
-  
-  // append all new ones
-  const newClassNames = splitClassName(stringClassNames);
-  currentClassNames = currentClassNames.concat(newClassNames);
+  // Find the classNames on the body that we haven't added
+  var currentClassNames = splitClassName(
+    document.body.className
+  ).filter(function(className) {
+    return BodyClassName.addedClassNames.indexOf(className) === -1;
+  });
 
-  // set body class name
-  document.body.className = currentClassNames.join(' ').trim();
+  // Add "untracked" classNames with our classNames
+  var newClassNames = currentClassNames
+    .concat(splitClassName(stringClassNames))
+    .map(function(str) {
+      return str.trim();
+    })
+    .filter(Boolean)
+    .join(' ');
 
-  // track which class names we added
-  classNameCache = newClassNames;
+  BodyClassName.addedClassNames = newClassNames;
+  document.body.className = newClassNames;
 }
 
-function BodyClassName(props){
+function BodyClassName(props) {
   if (props.children) {
     return React.Children.only(props.children);
   } else {
@@ -49,12 +48,12 @@ function BodyClassName(props){
   }
 }
 
+BodyClassName.addedClassNames = [];
 BodyClassName.displayName = 'BodyClassName';
 BodyClassName.propTypes = {
-  className: PropTypes.string.isRequired
+  className: PropTypes.string.isRequired,
 };
 
-module.exports = withSideEffect(
-  reducePropsToState,
-  handleStateChangeOnClient
-)(BodyClassName);
+module.exports = withSideEffect(reducePropsToState, handleStateChangeOnClient)(
+  BodyClassName
+);
